5f27bf9210eb34a7f51072b46997838f074aec14
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2019-07-01 00:09:28 +0530

    Such a hack, needs fixing

--- money.c
+++ money.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <windows.h>
 #include <unistd.h>
+#include <psapi.h>
+#include <tchar.h>
 
 /* 32-bit and 64-bit check to keep MEM size same as LPVOID. */
 #ifdef _WIN64
@@ -14,13 +16,13 @@
 
 
 int main() {
-    MEM baseaddress = 0x11D0000;
+    MEM baseaddress = 0xE90000;
     MEM solarian_offset = 0x7AD170;
     MEM result = baseaddress + solarian_offset;
     LPVOID address = (LPVOID)result;
     printf("%p\n", address);
 
-    double nVal = 0;
+    double nVal = 1000000;
     double value = 0;
     int stat = 1;
 
@@ -28,9 +30,26 @@ int main() {
     if(hWnd == 0){
         printf("Could not find window.\n");
     } else {
+    	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
         DWORD PID;
         GetWindowThreadProcessId(hWnd, &PID);
         HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, PID);
+
+        HMODULE hMod;
+        DWORD cbNeeded;
+
+        if (EnumProcessModulesEx(hProc, &hMod, sizeof(hMod),
+            &cbNeeded, LIST_MODULES_32BIT | LIST_MODULES_64BIT))
+        {
+            GetModuleBaseName(hProc, hMod, szProcessName,
+                sizeof(szProcessName) / sizeof(TCHAR));
+            //if (!_tcsicmp(processName, szProcessName)) {
+                printf("BASE: %p\n", hMod);
+                printf("RESULT: %p\n", hMod + solarian_offset);
+            //}
+        }
+
+        exit(1);
 
         if(!hProc) {
             printf("Cannot open process.\n");
